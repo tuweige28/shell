7e01d5f92c13952bf45c044fe9f57397b1e01543
tuweige28 <liuzheng28@126.com>
2020-07-08 16:08:49 +0800

    Delete create_batches_devops_users.sh

--- user_management/create_batches_devops_users.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-user=devops
-for ip in `cat /root/shell/host-ip`
-do
-   ssh $ip useradd $user
-   ssh $ip "echo 'c6^A[2#)b!L6*f3P{mT8(f0Q' | passwd --stdin $user";
-   ssh $ip chmod -R 775 /devops
-   ssh $ip usermod -a -G root $user
-done
